cdb6605a2db4f425205663236cf9fc407a800a44
Hu Sheng <tommylikehu@gmail.com>
2021-03-27 15:45:42 +0800

    Revert "change Theme reco"

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    theme: 'reco',
     title: 'opensourceway',
     description: 'official website for opensourceway',
     base: '/',
@@ -7,9 +6,9 @@ module.exports = {
     markdown: {
         lineNumbers: true
     },
+    port: 8086,
     head: [['link',
-        {rel: 'icon', href: '/img/logo.png'}],
-        ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no'}]],
+        {rel: 'icon', href: 'img/logo.jpg'}]],
     plugins: [['markdown-it-toc-done-right'], ['@goy/svg-icons']],
     themeConfig: {
         logo: '/img/logo.png',
@@ -18,20 +17,10 @@ module.exports = {
             {text: 'Service Guides', link: '/service/'},
             {text: 'FAQ', link: '/faq/'},
             {text: 'Contact us', link: '/contact/faq'},
-            {
-                text: 'More',
-                items: [
-                    {text: 'Tag', link: '/tag/', icon: 'reco-tag'}
-                ]
-            }
         ],
-        subSidebar: 'auto',
+        sidebar: 'auto',
         sidebarDepth: 2,
-        modePicker: false,
         search: false,
         footer: true,
-        record: '蜀ICP备20009468号-1',
-        recordLink: 'https://icp.chinaz.com/home/info?host=osinfra.cn',
-        startYear: '2021',
-    }
+    },
 }
